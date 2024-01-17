65179e6f5ba3a4959c7077069630dccf7f71305b
aldimaulana1605 <aldimaulana160503@gmail.com>
2024-01-17 21:45:43 +0700

    p

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,6 +18,67 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        User::create([
+            'name' => 'Aldi Maulana Bahari',
+            'email' => 'aldimaulana160503@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+
+        User::create([
+            'name' => 'Doddy Ferdiansyah',
+            'email' => 'doddycuy@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+
+        Category::create([
+            'name' => 'Web Programming',
+            'slug' => 'web-programming'
+        ]);
+
+        Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        Post::create([
+            'title' => 'Judul Pertama',
+            'slug' => 'judul-pertama',
+            'excerpt' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam voluptatum maiores distinctio exercitationem',
+            'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam voluptatum maiores distinctio exercitationem, itaque cupiditate quidem illo ut voluptate aspernatur pariatur nostrum sapiente quia vitae doloribus, ratione ipsam eius ipsa neque, eligendi obcaecati doloremque facilis. Mollitia, expedita. Quisquam molestiae quas, repudiandae facilis qui, voluptas nulla id fugit magni rerum ipsam nihil necessitatibus iure earum doloremque commodi praesentium at excepturi amet magnam delectus? Ut, magnam facere. Repellendus totam ea hic, culpa sunt praesentium quisquam doloremque, at commodi itaque expedita mollitia beatae numquam recusandae laborum non in! Molestias, quaerat amet eaque fugit autem assumenda, corrupti impedit unde quis laboriosam, eius quam aperiam!',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ke Dua',
+            'slug' => 'judul-ke-dua',
+            'excerpt' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam voluptatum maiores distinctio exercitationem',
+            'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam voluptatum maiores distinctio exercitationem, itaque cupiditate quidem illo ut voluptate aspernatur pariatur nostrum sapiente quia vitae doloribus, ratione ipsam eius ipsa neque, eligendi obcaecati doloremque facilis. Mollitia, expedita. Quisquam molestiae quas, repudiandae facilis qui, voluptas nulla id fugit magni rerum ipsam nihil necessitatibus iure earum doloremque commodi praesentium at excepturi amet magnam delectus? Ut, magnam facere. Repellendus totam ea hic, culpa sunt praesentium quisquam doloremque, at commodi itaque expedita mollitia beatae numquam recusandae laborum non in! Molestias, quaerat amet eaque fugit autem assumenda, corrupti impedit unde quis laboriosam, eius quam aperiam!',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ke Tiga',
+            'slug' => 'judul-ke-tiga',
+            'excerpt' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam voluptatum maiores distinctio exercitationem',
+            'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam voluptatum maiores distinctio exercitationem, itaque cupiditate quidem illo ut voluptate aspernatur pariatur nostrum sapiente quia vitae doloribus, ratione ipsam eius ipsa neque, eligendi obcaecati doloremque facilis. Mollitia, expedita. Quisquam molestiae quas, repudiandae facilis qui, voluptas nulla id fugit magni rerum ipsam nihil necessitatibus iure earum doloremque commodi praesentium at excepturi amet magnam delectus? Ut, magnam facere. Repellendus totam ea hic, culpa sunt praesentium quisquam doloremque, at commodi itaque expedita mollitia beatae numquam recusandae laborum non in! Molestias, quaerat amet eaque fugit autem assumenda, corrupti impedit unde quis laboriosam, eius quam aperiam!',
+            'category_id' => 2,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ke Empat',
+            'slug' => 'judul-ke-empat',
+            'excerpt' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam voluptatum maiores distinctio exercitationem',
+            'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam voluptatum maiores distinctio exercitationem, itaque cupiditate quidem illo ut voluptate aspernatur pariatur nostrum sapiente quia vitae doloribus, ratione ipsam eius ipsa neque, eligendi obcaecati doloremque facilis. Mollitia, expedita. Quisquam molestiae quas, repudiandae facilis qui, voluptas nulla id fugit magni rerum ipsam nihil necessitatibus iure earum doloremque commodi praesentium at excepturi amet magnam delectus? Ut, magnam facere. Repellendus totam ea hic, culpa sunt praesentium quisquam doloremque, at commodi itaque expedita mollitia beatae numquam recusandae laborum non in! Molestias, quaerat amet eaque fugit autem assumenda, corrupti impedit unde quis laboriosam, eius quam aperiam!',
+            'category_id' => 2,
+            'user_id' => 2
+        ]);
+
+
+
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,12 +3,12 @@
 @section('container')
     <h1 class="mb-5">{{ $post->title }}</h1>
 
-    <p>By. Aldi Maulana Bahari in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+    <p>By. <a href="#" class="text-decoration-none">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
 
     {{-- <h5>{{ $post["author"] }}</h5> --}}
 
     {!! $post->body !!}
 
-<a href="/posts">Back to Posts</a>
+<a href="/posts" class="d-block mt-3">Back to Posts</a>
 
 @endsection
