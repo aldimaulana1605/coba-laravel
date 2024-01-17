0052baa31cb0090afa114fcaa177b1773ac35e77
aldimaulana1605 <aldimaulana160503@gmail.com>
2024-01-17 21:01:00 +0700

    p

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,7 +1,9 @@
 @extends('layouts.main')
 
 @section('container')
-    <h1 class="mb-5">{{ $post->title }}</h2>
+    <h1 class="mb-5">{{ $post->title }}</h1>
+
+    <p>By. Aldi Maulana Bahari in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
 
     {{-- <h5>{{ $post["author"] }}</h5> --}}
 
