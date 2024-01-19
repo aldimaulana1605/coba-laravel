5f2bb5101b0b5b1a5e4beddabcb01fa4f8c6f818
aldimaulana1605 <aldimaulana160503@gmail.com>
2024-01-19 07:46:51 +0700

    p

--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -8,19 +8,11 @@
         <div class="row">
             @foreach ($categories as $category)
             <div class="col-md-4">
-                <a href="/categories/{{ $category->slug }}">
+                <a href="/posts?category={{ $category->slug }}">
                     <div class="card bg-dark text-white">
-                        {{-- <div class="position-absolute px-3 py-2 text-white" style="background-color: rgba(0, 0, 0, 0.7)"><a href="/categories/{{ $post->category->slug }}" class="text-white text-decoration-none">{{ $post->category->name }}</a></div> --}}
                         <img src="https://source.unsplash.com/500x500?{{ $category->name }}" class="card-img" alt="{{ $category->name }}">
                         <div class="card-img-overlay d-flex align-items-center p-0">
                             <h5 class="card-title text-center flex-fill p-4 fs-3" style="background-color: rgba(0, 0, 0, 0.7)">{{ $category->name }}</h5>
-                            {{-- <p>
-                                <small class="text-muted">
-                                <p>By. <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> {{ $post->created_at->diffForHumans() }}
-                                </small>
-                                </p>
-                            <p class="card-text">{{ $post->excerpt }}</p>
-                            <a href="/posts/{{ $post->slug }}" class="btn btn-primaru">Read more</a> --}}
                         </div>
                     </div>
                 </a>
